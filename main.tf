a290031f8a927048d5792d9c62adc3e091fea910
Jeremy Giddings <jeremy.giddings@netapp.com>
2025-12-08 10:19:37 +1100

    use ic log storage

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ resource "aws_s3_bucket_cors_configuration" "origin" {
 }
 
 module "logs" {
-  source                     = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=tags/1.0.0"
+  source                     = "git::https://github.com/instaclustr/terraform-aws-s3-log-storage.git?ref=ic-console2-dns"
   namespace                  = var.namespace
   stage                      = var.stage
   name                       = var.name
