a7e2c4d85960b9fcf60185e81bdfeb95b0c97406
Jeremy Giddings <jeremy.giddings@netapp.com>
2025-12-11 12:30:19 +1100

    put back attributes

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "origin_label" {
   stage      = var.stage
   name       = var.name
   delimiter  = var.delimiter
-  attributes = var.attributes
+  attributes = compact(concat(var.attributes, ["origin"]))
   tags       = var.tags
 }
 
@@ -101,7 +101,7 @@ module "logs" {
   stage                      = var.stage
   name                       = var.name
   delimiter                  = var.delimiter
-  attributes                 = var.attributes
+  attributes                 = compact(concat(var.attributes, ["logs"]))
   tags                       = var.tags
   standard_transition_days   = var.log_standard_transition_days
   glacier_transition_days    = var.log_glacier_transition_days
